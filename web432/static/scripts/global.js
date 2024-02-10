9405229c57d38dea9311efbccd42656fa8fea340
Ruben Garcia <rubenjmgarcia@hotmail.com>
2024-02-10 20:32:13 -0100

    Loading screen added

--- web432/static/scripts/global.js
+++ web432/static/scripts/global.js
@@ -1,1 +1,12 @@
 /* global */
+
+/* loading */
+$(window).on("load", function(){
+    $('.loading-logo').removeClass('animated');
+    $('.loading').addClass('loaded').fadeOut(500);
+});
+
+setTimeout(function(){
+    $('.loading-logo').removeClass('animated');
+    $('.loading').addClass('loaded').fadeOut(500);
+}, 2500);
